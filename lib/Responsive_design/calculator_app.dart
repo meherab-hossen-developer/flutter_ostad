0b7af72e276146b76c20f4dc74e479ca7841e23b
Meherab Hossen <mhnnishat0102@gmail.com>
2026-03-08 03:27:47 +0600

    Calculator app completion

--- lib/Responsive_design/calculator_app.dart
+++ lib/Responsive_design/calculator_app.dart
@@ -9,48 +9,106 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+
+  String _output = '0';
+  String _input = '0';
+  String _operator = '';
+  double num1 = 0;
+  double num2 = 0;
+
+  void buttonPress(String value){
+    print('int value = $value');
+    setState(() {
+
+      if(value == 'C'){
+
+        _output = '0';
+        _input = '0';
+        _operator = '';
+        num1 = 0;
+        num2 = 0;
+      }else if(value == '='){
+        num2 = double.parse(_input);
+        if(_operator == '+'){
+          _output = (num1 + num2).toString();
+        }else if(_operator == '-'){
+          _output = (num1 - num2).toString();
+        }else if(_operator == '*'){
+          _output = (num1 * num2).toString();
+        }else if(_operator == '÷'){
+          _output = num2 != 0 ? (num1 / num2).toString() : 'Error';
+        }
+      }else if(['+','-','*','÷'].contains(value)){
+        num1 = double.parse(_input);
+        _operator = value;
+        _input = '0';
+      }else{
+        if(_input == '0'){
+          _input = value;
+        }else{
+          _input += value;
+        }
+        _output = _input;
+      }
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(_output,style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                ),),
+              ),
+            ),
+          ),
           Row(
             children: [
-              calcButton(onClick: (){}, text: '7'),
-              calcButton(onClick: (){}, text: '8'),
-              calcButton(onClick: (){}, text: '9'),
-              calcButton(onClick: (){}, text: '÷', color: Colors.orange,),
+              calcButton(onClick: ()=>buttonPress('7'), text: '7'),
+              calcButton(onClick: ()=>buttonPress('8'), text: '8'),
+              calcButton(onClick: ()=>buttonPress('9'), text: '9'),
+              calcButton(onClick: ()=>buttonPress('÷'), text: '÷', color: Colors.orange,),
 
             ],
           ),
           Row(
             children: [
-              calcButton(onClick: (){}, text: '4'),
-              calcButton(onClick: (){}, text: '5'),
-              calcButton(onClick: (){}, text: '6'),
-              calcButton(onClick: (){}, text: '×', color: Colors.orange,),
+              calcButton(onClick: ()=>buttonPress('4'), text: '4'),
+              calcButton(onClick: ()=>buttonPress('5'), text: '5'),
+              calcButton(onClick: ()=>buttonPress('6'), text: '6'),
+              calcButton(onClick: ()=>buttonPress('*'), text: '×', color: Colors.orange,),
+            ],
+          ),
+          Row(
+            children: [
+              calcButton(onClick: ()=>buttonPress('1'), text: '1'),
+              calcButton(onClick: ()=>buttonPress('2'), text: '2'),
+              calcButton(onClick: ()=>buttonPress('3'), text: '3'),
+              calcButton(onClick: ()=>buttonPress('-'), text: '-', color: Colors.orange,),
 
             ],
           ),
           Row(
             children: [
-              calcButton(onClick: (){}, text: '1'),
-              calcButton(onClick: (){}, text: '2'),
-              calcButton(onClick: (){}, text: '3'),
-              calcButton(onClick: (){}, text: '+', color: Colors.orange,),
+              calcButton(onClick: ()=>buttonPress('C'), text: 'C'),
+              calcButton(onClick: ()=>buttonPress('0'), text: '0'),
+              calcButton(onClick: ()=>buttonPress('='), text: '='),
+              calcButton(onClick: ()=>buttonPress('+'), text: '+', color: Colors.orange,),
 
             ],
           ),
-          Row(
-            children: [
-              calcButton(onClick: (){}, text: '7'),
-              calcButton(onClick: (){}, text: '8'),
-              calcButton(onClick: (){}, text: '9'),
-              calcButton(onClick: (){}, text: '±', color: Colors.orange,),
-
-            ],
-          ),
+          SizedBox(height: 60),
         ],
       ),
     );
--- lib/Responsive_design/widget/calcButton.dart
+++ lib/Responsive_design/widget/calcButton.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/material.dart';
 class calcButton extends StatelessWidget {
-  String text;
-  Color? color;
+  final String text;
+  final Color? color;
   final VoidCallback onClick;
+  final double? font;
 
   calcButton({
     super.key,
     required this.onClick,
     this.color,
-    required this.text
+    required this.text,
+    this.font
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(5),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.all(28),
-          shape: CircleBorder(),
-          //padding: const EdgeInsets.all(30),
-          backgroundColor: color ?? Color.fromRGBO(43, 47, 48, 1.0),
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(5),
+        child: SizedBox(
+          height: 90,
+          width: 90,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              backgroundColor: color ?? Color.fromRGBO(43, 47, 48, 1.0),
+            ),
+            onPressed: onClick,
+            child: Text(text, style: TextStyle(
+              fontSize: font ?? 60,
+              color: Colors.white,
+            ),),
+          ),
         ),
-        onPressed: onClick,
-        child: Text(text, style: TextStyle(
-          fontSize: 60,
-          color: Colors.white,
-        ),),
       ),
     );
   }
