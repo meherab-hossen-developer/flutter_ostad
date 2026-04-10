aee34bbdcfde4b2640b1ee6b7b04e4f5d58a2f73
Meherab Hossen <mhnnishat0102@gmail.com>
2026-04-10 19:22:13 +0600

    Error fix

--- lib/Theme and navigation/app.dart
+++ lib/Theme and navigation/app.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
 
       routes: {
-        '/': (context) => MoneyManagement(),
+        '/': (context) => MoneyManagement()
       },
     );
   }
